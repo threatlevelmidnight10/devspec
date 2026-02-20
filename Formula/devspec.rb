69a09496a2b90dfa2524976d210b99ee2a64e8ac
threatlevelmidnight10 <ravikumarabhishek10@gmail.com>
2026-02-20 16:38:03 +0530

    build: publish stable homebrew formula for devspec

--- Formula/devspec.rb
+++ Formula/devspec.rb
@@ -1,6 +1,8 @@
 class Devspec < Formula
   desc "Deterministic CLI orchestration for Cursor agent workflows"
   homepage "https://github.com/threatlevelmidnight10/devspec"
+  url "https://github.com/threatlevelmidnight10/devspec/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "dccdef64eb02cd611ccdd7d85bb64f60dbd44e47170b492223675fb21a7716ce"
   head "https://github.com/threatlevelmidnight10/devspec.git", branch: "main"
 
   depends_on "go" => :build
